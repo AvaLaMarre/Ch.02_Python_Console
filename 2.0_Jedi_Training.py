45cbf29fe3ba3db18a29df05b978f79518d9ccb6
lamarreava <lamarreava@urbandaleschools.com>
2023-09-18 19:58:52 -0500

    First commit

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,25 +1,25 @@
 '''
-2.0 Jedi Training (20pts)  Name:________________
+2.0 Jedi Training (20pts)  Name:__Ava_LaMarre_____________
 
 UPLOAD YOUR CH 2 PDF LOG! (6pts)
 
 1.) How do you enter a single line comment in a program? Give an example. (1pt)
-
+#The pound sign is how to make a signle line comment
 
 2.) If a=2 and b=5, predict all of the following below and record your predictions. (9pts)
 Then in the Python Console window, enter a=2 and b=5 and then all of the following and record the actual output.
 If the output is an error record the error and try to determine what the error is!
 Comment about any of your predictions that didn't match the actual output
 
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
+b/a                 = b divided by a, 5/2=2.5
+b//a                =2, did not know what it did before, no clue
+b**a                =25, I did not think that ** was seting it to the power
+b%a                 =1
+a+B                 =B is not defined, NameError: name 'B' is not defined
+type(42)            = Before I might of thought that it would type out the items in the (), class'int'
+type(42.0)          = <class 'flout'>
+type("C3PO")        =<class 'string'>
+type(True)          = <class 'bool'>
 
 
 3.) Predict what would be the final output of (a) and type(a) if you enter the following 5 lines        (2pts)
@@ -31,8 +31,8 @@ a*=10
 a/=2
 a+=12
 a-=7
-a             =
-type(a)       =
+a             = Prediction(15), Actual(15.0)
+type(a)       = Prediction) <Class 'int'>), Actual(<class 'flout'>)
 
 
 4.) What is the mistake in the following code? Comment it and fix it!  (1pt)
@@ -41,6 +41,9 @@ x,y = (4,5)
 a = 3(x + y)
 a
 
+() does not multiply, it need *
+a = 3*(x + y
+a
 
 5.) What is the mistake in the following code so it will calculate the average? Comment it and fix it!  (1pt)
 
@@ -48,5 +51,8 @@ x,y,z =(3,4,5)
 ave = x+y+z/3
 ave
 
+There is no ()
+ave = (x+y+z)/3
+ave
 
 '''
